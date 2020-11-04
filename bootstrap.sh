89f2881cdabb9bb5d2dba3d5f4730615c76cd51d
Ismael Arias <ismaelariasmn@gmail.com>
2020-11-04 23:53:41 +0100

    Using jaredhoccut Ansible's role to install Gnome extensions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ PLAYBOOK="$ROOTDIR/installation.yml"
 
 dnf upgrade && dnf install -y ansible
 
+ansible-galaxy install --roles-path . jaredhocutt.gnome_extensions
 ansible-playbook "$PLAYBOOK" --ask-become-pass
 
 exit 0
