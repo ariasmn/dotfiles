88987d80d8761e62d8216ac66323a2350ca5bc1b
Ismael Arias <ismaelariasmn@gmail.com>
2021-07-03 20:25:48 +0200

    Starting the migration to Debian+KDE. Still need to fully test the Ansible playbook, KDE's config file and update the README.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,8 @@ set -e
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PLAYBOOK="$ROOTDIR/installation.yml"
 
-dnf upgrade && dnf install -y ansible
+apt update && apt install -y ansible
 
-ansible-galaxy install --roles-path "$ROOTDIR" jaredhocutt.gnome_extensions
 ansible-playbook "$PLAYBOOK" --ask-become-pass
 
 exit 0
